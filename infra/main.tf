81a13a37b64a8f2e0de21e82353a30d37725379e
Robert Amoah <mr_robertamoah@yahoo.com>
2025-06-30 15:18:54 +0000

    Add infrastructure for 3-tier app.

---
+++ infra/main.tf
@@ -0,0 +1,82 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_name = "lampstack-vpc"
+  vpc_cidr = "10.0.0.0/16"
+}
+
+module "security" {
+  source = "./modules/security"
+
+  vpc_name   = module.vpc.vpc_name
+  vpc_id     = module.vpc.vpc_id
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+module "bastion" {
+  source = "./modules/ec2"
+
+  vpc_id             = module.vpc.vpc_id
+  ami_id             = data.aws_ami.ubuntu.id
+  instance_type      = var.instance_type
+  subnet_id          = module.vpc.public_subnet_ids[0]
+  security_group_ids = [module.security.bastion_sg_id]
+  key_name           = module.security.key_name
+  tags = {
+    Name = "${module.vpc.vpc_name}-bastion"
+  }
+}
+
+module "web" {
+  source = "./modules/ec2"
+
+  vpc_id             = module.vpc.vpc_id
+  ami_id             = data.aws_ami.ubuntu.id
+  instance_type      = var.instance_type
+  subnet_id          = module.vpc.public_subnet_ids[0]
+  security_group_ids = [module.security.web_sg_id]
+  tags = {
+    Name = "${module.vpc.vpc_name}-web"
+  }
+
+  user_data = <<-EOF
+        #!/bin/bash
+        apt-get update
+        apt-get install -y apache2
+        systemctl enable apache2
+        systemctl start apache2
+        EOF
+}
+
+module "app" {
+  source = "./modules/ec2"
+
+  vpc_id             = module.vpc.vpc_id
+  ami_id             = data.aws_ami.ubuntu.id
+  instance_type      = var.instance_type
+  subnet_id          = module.vpc.private_subnet_ids[0]
+  security_group_ids = [module.security.app_sg_id]
+  tags = {
+    Name = "${module.vpc.vpc_name}-app"
+  }
+
+  user_data = <<-EOF
+        #!/bin/bash
+        apt-get update
+        apt-get install -y php libapache2-mod-php
+        systemctl restart apache2
+        EOF
+}
+
+module "db" {
+  source = "./modules/ec2"
+
+  vpc_id             = module.vpc.vpc_id
+  ami_id             = data.aws_ami.ubuntu.id
+  instance_type      = var.instance_type
+  subnet_id          = module.vpc.private_subnet_ids[1]
+  security_group_ids = [module.security.db_sg_id]
+  tags = {
+    Name = "${module.vpc.vpc_name}-db"
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "profile" {
+  type    = string
+  default = "sandbox"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
